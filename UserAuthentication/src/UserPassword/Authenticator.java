5800337439e641f9b3ae7cabf52395061d0cc640
MattMontadaSTU <60233452+MattMontadaSTU@users.noreply.github.com>
2020-02-11 12:58:53 -0500

    Update Authenticator.java

--- UserAuthentication/src/UserPassword/Authenticator.java
+++ UserAuthentication/src/UserPassword/Authenticator.java
@@ -17,7 +17,7 @@ public class Authenticator {
 	 * @param args
 	 * @throws Exception
 	 */
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
 		System.out.print("Which action do you want to perform:"
